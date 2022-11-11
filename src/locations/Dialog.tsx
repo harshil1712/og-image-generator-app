d661515678d230b8e2598c4810750153d6abed11
Harshil Agrawal (harshil1712) <github@harshil.dev>
2022-11-11 12:32:39 +0100

    🔨 Fix upload asset code

--- src/locations/Dialog.tsx
+++ src/locations/Dialog.tsx
@@ -4,23 +4,24 @@ import { DialogExtensionSDK } from '@contentful/app-sdk';
 import { useCMA, useAutoResizer, useSDK } from '@contentful/react-apps-toolkit';
 import satori from 'satori';
 import '../style.css'
-import toImg from 'react-svg-to-image';
+const toImg = require('react-svg-to-image')
+
+declare global {
+  interface Window {
+    __resource?: any;
+  }
+}
 
 const Dialog = () => {
   const sdk = useSDK();
-  /*
-     To use the cma, inject it as follows.
-     If it is not needed, you can remove the next line.
-  */
   const cma = useCMA();
-  // const space = cma.
-  // console.log(space);
   useAutoResizer();
 
-  const [svgHtml, setSvgHtml] = useState('');
+  const [svgHtml, setSvgHtml] = useState<any>();
 
   useEffect(()=>{
     const generateOg = async () =>{
+      if (window === undefined) return []
       const [font, fontBold, fontIcon, Segmenter] = 
       window.__resource ||
       (window.__resource = await Promise.all([
@@ -56,19 +57,10 @@ const Dialog = () => {
     const fetchImage = await fetch(pngImage);
     const pngData = await fetchImage.arrayBuffer();
     const pngFile = await new File([pngData], fileName, {type: fileType});
-    const {sys} = await cma.space.get();
+    const {sys} = await cma.space.get({});
     const {id} = await sys;
 
-    console.log(pngImage)
-
-    const upload = await cma.upload.create({
-      spaceId: id
-    },
-    {
-      file: pngData
-    });
-
-    const linkAsset = await cma.asset.create({
+    const linkAsset = await cma.asset.createFromFiles({
       spaceId: id
     },
     {
@@ -76,30 +68,21 @@ const Dialog = () => {
         title: {
           "en-US": id
         },
+        description: {
+          "en-US": ""
+        },
         file: {
           'en-US': {
-            uploadFrom: {
-              sys: {
-                type: 'Link',
-                linkType: 'Upload',
-                id: upload.sys.id
-              }
-            },
+            file: pngData,
+            fileName: fileName,
             contentType: 'image/png',
-            // upload: pngImage
           }
         }
       }
     }
     )
 
-
-    const {sys:assetSys} = await linkAsset;
-    const {id:assetId} = await assetSys;
-    console.log(await linkAsset);
-    //const processImage = await cma.asset.processForAllLocales({spaceId:id}, linkAsset, 'en-US')
-    // const publishImage = await processImage.publish();
-    //console.log(processImage)
+    const processImage = await cma.asset.processForAllLocales({spaceId:id}, linkAsset)
     };
 
   return (
